d6bb1071790fb2c77509264b18f6baa06adc767b
Andreea Racovita <andreearacovita@Andreeas-MacBook-Air.local>
2023-03-15 20:20:15 +0200

    Initial commit

---
+++ src/main/java/com/andreearacovita/ebankingrestapi/EbankingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.andreearacovita.ebankingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreearacovita/ebankingrestapi/EbankingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreearacovita.ebankingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
